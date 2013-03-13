f92597471292a90741f4a1e54d71c6f5a00d49fa
Andrews Medina <andrewsmedina@gmail.com>
2013-03-13 16:53:00 -0300

    implemented the namefier.

--- namefier.py
+++ namefier.py
@@ -8,3 +8,19 @@ def ec2_connection():
 
 def mongo_connection():
     return pymongo.MongoClient('localhost', 27017)
+
+
+def namefier(apps):
+    ec2 = ec2_connection()
+    for app in apps:
+        for unit in app["units"]:
+            ec2.create_tags([unit["instanceid"]], {"name": unit["name"]})
+
+
+def main():
+    apps = mongo_connection().tsuru.apps.find()
+    namefier(apps)
+
+
+if __name__ == "__main__":
+    main()
--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 import unittest
 import mock
 
-from namefier import ec2_connection, mongo_connection
+from namefier import ec2_connection, mongo_connection, namefier
 
 
 class Ec2Test(unittest.TestCase):
@@ -15,5 +15,19 @@ class MongoTest(unittest.TestCase):
     @mock.patch("pymongo.MongoClient")
     def test_mongo_connection(self, conn):
         mongo_connection()
+
+
+class NamefierTest(unittest.TestCase):
+    @mock.patch("boto.connect_ec2")
+    def test_namefier(self, ec2):
+        conn = ec2.return_value
+        apps = [{
+            "units": [
+                {"name": "vm1", "instanceid": "i-1"}
+            ]
+        }]
+        namefier(apps)
+        conn.create_tags.assert_called_with(["i-1"], {"name": "vm1"})
+
 
 unittest.main()
